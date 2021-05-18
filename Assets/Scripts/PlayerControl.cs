76b0744f23edfd17131d478a298dc2f42b975c2d
NamTaeHee1 <skaxogml777@naver.com>
2021-05-18 10:01:22 +0900

    [ADD] Tile + [UPDATE] PlayerControl
    [ADD] Tile + [UPDATE] PlayerControl

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,9 +7,30 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private Transform ICE_Transform, FIRE_Transform;
     [SerializeField] private GameObject ICE_GameObject, FIRE_GameObejct;
     [SerializeField] private float RotateSpeed = 3.0f;
+    [SerializeField] private Vector2 RotateDirection = Vector3.back;
+
 
     void Update()
     {
-        ICE_Transform.RotateAround(FIRE_Transform.position ,Vector3.back, RotateSpeed * Time.deltaTime);
+        MovePlanet();
+        RotatePlanet();
+    }
+
+    void RotatePlanet()
+    {
+        ICE_Transform.RotateAround(FIRE_Transform.position, Vector3.back, RotateSpeed * Time.deltaTime);
+    }
+
+    void MovePlanet()
+    {
+        if(Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+
+    void ShowOrbit()
+    {
+        if()
     }
 }
